f6856883cf8c05f7787b717d8ed2b231cc768931
alex <a.grigorenko.belvg@gmail.com>
2018-12-03 17:31:36 +0300

    remove code before refactoring

--- Billmate/Logger/Logger.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Billmate\Billmate\Logger;
-
-class Logger extends \Monolog\Logger
-{
-}
--- BillmateCheckout/Controller/Index/Index.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Billmate\BillmateCheckout\Controller\Index;
-class Index extends \Magento\Checkout\Controller\Index\Index {
-}
